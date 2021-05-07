239e3ff128cdf6daf6bd1220ed8510f75344d684
DESKTOP-FMKN3I3\ohmym <ohmymoon@gmail.com>
2021-05-07 09:50:45 +0900

    Initial commit

---
+++ src/main/java/xyz/ohmymoon/moonproject/MoonProjectApplication.java
@@ -0,0 +1,13 @@
+package xyz.ohmymoon.moonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/ohmymoon/moonproject/MoonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ohmymoon.moonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
